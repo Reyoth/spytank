a606cc092b5094c46014b2a0ba7088a53514d7e4
Reyoth <serialkiwi@gmail.com>
2018-06-25 16:11:48 +0200

    Exo1

--- basic_robot_control/basic_robot.py
+++ basic_robot_control/basic_robot.py
@@ -16,26 +16,4 @@ from gopigo import *	#Contient les fonctions de base pour controler le SpyTank
 import sys				#Contient la fonction exit pour sortir du programme
 
 print ("Press:\n\tz: Pour avancer\n\tq: Pour Tourner a gauche\n\td: Pour Tourner a droite\n\ts: Pour Reculer\n\tx: Pour Stopper\n\tw: Pour sortir du programme\n")
-while True:
-	print ("Entrez une commande:")
-	a=input()	# Lire l'entree clavier ( equivaut a input() en python3 )
-	if a=='z':
-		fwd()	# Avancer (forward)
-	elif a=='q':
-		left()	# Tourner a gauche
-	elif a=='d':
-		right()	# Tourner a droite
-	elif a=='s':
-		bwd()	# Reculer (backward)
-	elif a=='x':
-		stop()	# Stop
-	elif a=='t':			
-		increase_speed()	# Augmenter la vitesse
-	elif a=='g':
-		decrease_speed()	# Diminuer la vitesse
-	elif a=='w':
-		print ("Arret du programme")		# Exit
-		sys.exit()
-	else:
-		print ("Mauvaise commande, reessayez")
-	time.sleep(.1)
+
