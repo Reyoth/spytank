23c0bc99b9163ab9dea678bf599fe5576e7588fe
Reyoth <serialkiwi@gmail.com>
2018-06-25 13:53:14 +0200

    fix basic_robot_control : python3, accent

--- basic_robot_control/basic_robot.py
+++ basic_robot_control/basic_robot.py
@@ -1,41 +1,41 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
                                                           
 # Exemple simple pour controler le SpyTank
 # Controls:
 # 	z:	Avancer
-#	q:	Tourner à gauche
-#	d:	Tourner à droite
+#	q:	Tourner a gauche
+#	d:	Tourner a droite
 #	s:	Reculer
 #	x:	Stop
-#	t:	Augmenter la vitesse	(si implémenté)
-#	g:	Diminuer la vitesse		(si implémenté)
+#	t:	Augmenter la vitesse	(si implemente)
+#	g:	Diminuer la vitesse		(si implemente)
 #	w: 	Sortir du programme
 
 
 from gopigo import *	#Contient les fonctions de base pour controler le SpyTank
 import sys				#Contient la fonction exit pour sortir du programme
 
-print "Press:\n\tz: Pour avancer\n\tq: Pour Tourner à gauche\n\td: Pour Tourner à droite\n\ts: Pour Reculer\n\tx: Pour Stopper\n\tw: Pour sortir du programme\n"
+print ("Press:\n\tz: Pour avancer\n\tq: Pour Tourner a gauche\n\td: Pour Tourner a droite\n\ts: Pour Reculer\n\tx: Pour Stopper\n\tw: Pour sortir du programme\n")
 while True:
-	print "Entrez une commande:",
-	a=raw_input()	# Lire l'entrée clavier ( équivaut à input() en python3 )
+	print ("Entrez une commande:")
+	a=input()	# Lire l'entree clavier ( equivaut a input() en python3 )
 	if a=='z':
 		fwd()	# Avancer (forward)
 	elif a=='q':
-		left()	# Tourner à gauche
+		left()	# Tourner a gauche
 	elif a=='d':
-		right()	# Tourner à droite
+		right()	# Tourner a droite
 	elif a=='s':
 		bwd()	# Reculer (backward)
 	elif a=='x':
 		stop()	# Stop
-	#elif a=='t':			
-		#increase_speed()	# Augmenter la vitesse
-	#elif a=='g':
-		#decrease_speed()	# Diminuer la vitesse
+	elif a=='t':			
+		increase_speed()	# Augmenter la vitesse
+	elif a=='g':
+		decrease_speed()	# Diminuer la vitesse
 	elif a=='w':
-		print "Arret du programme"		# Exit
+		print ("Arret du programme")		# Exit
 		sys.exit()
 	else:
-		print "Mauvaise commande, réessayez"
+		print ("Mauvaise commande, reessayez")
 	time.sleep(.1)
