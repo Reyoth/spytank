a3721b00eb86ed4ef3f05e731dfa4269d6ce9a0a
Jean <ujuppype-2147@yopmail.com>
2018-06-28 11:41:24 +0200

    update gopigo

--- basic_robot_control/basic_robot.py
+++ basic_robot_control/basic_robot.py
@@ -16,19 +16,20 @@ from gopigo import *	#Contient les fonctions de base pour controler le SpyTank
 import sys				#Contient la fonction exit pour sortir du programme
 
 print ("Press:\n\tz: Pour avancer\n\tq: Pour Tourner a gauche\n\td: Pour Tourner a droite\n\ts: Pour Reculer\n\tx: Pour Stopper\n\tw: Pour sortir du programme\n")
+vitesse=80
 while True:
 	print ("Entrez une commande:")
 	a=input()	# Lire l'entree clavier ( equivaut a input() en python3 )
 	if a=='z':
-		fwd()	# Avancer (forward)
+		fwd(vitesse)	# Avancer (forward)
 	elif a=='q':
-		left()	# Tourner a gauche
+		left(vitesse)	# Tourner a gauche
 	elif a=='d':
-		right()	# Tourner a droite
+		right(vitesse)	# Tourner a droite
 	elif a=='s':
-		bwd()	# Reculer (backward)
+		bwd(vitesse)	# Reculer (backward)
 	elif a=='x':
-		stop()	# Stop
+		fwd(0)	# Stop
 	elif a=='t':			
 		increase_speed()	# Augmenter la vitesse
 	elif a=='g':
